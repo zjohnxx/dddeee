f2856db0a663f84df9a026eb845ff66421096505
zjohnxx <46588717+zjohnxx@users.noreply.github.com>
2019-01-25 03:26:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '&';
+const prefix = '$';
 const axios = require('axios');
 const fs = require('fs');
 const ms = require('ms');
